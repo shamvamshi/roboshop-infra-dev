b0d71a4e48a5b2bfe0246a6d2b27331ac0df32d5
shamvamshi <shamvamshiwork@gmail.com>
2025-09-10 09:25:41 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "components" {
-    for_each = var.components
-  source = "../../terraform-aws-roboshop"
+  for_each = var.components
+  source = "git::https://github.com/shamvamshi/terraform-aws-roboshop.git?ref=main"
   component = each.key
   rule_priority = each.value.rule_priority
 }
