a9ab5793cd164f83df6da2ecdecb3da0d45f759c
shamvamshi <shamvamshiwork@gmail.com>
2025-09-19 16:38:27 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "components" {
   for_each = var.components
   source = "git::https://github.com/shamvamshi/terraform-aws-roboshop.git?ref=main"
-  component = each.key
+  components = each.key
   rule_priority = each.value.rule_priority # so each value is rule_priority, so u define value using (.)
 }
