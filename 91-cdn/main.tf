c6a67d3eeb2825d9ec6bcf1d3a25767a13e9d1a2
shamvamshi <shamvamshiwork@gmail.com>
2025-09-22 23:38:40 +0530

    terraform

--- 91-cdn/main.tf
+++ 91-cdn/main.tf
@@ -5,7 +5,7 @@ resource "aws_cloudfront_distribution" "roboshop" {
         http_port              = 80 // Required to be set but not used
         https_port             = 443
         origin_protocol_policy = "https-only"
-        origin_ssl_protocols   = ["TLSv1.2", "TLSv1.3"]
+        origin_ssl_protocols   = ["TLSv1.2"]
     }
     origin_id                = "cdn.${var.zone_name}"
   }
@@ -19,18 +19,9 @@ resource "aws_cloudfront_distribution" "roboshop" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "cdn.${var.zone_name}"
 
-    forwarded_values {
-      query_string = false
 
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    viewer_protocol_policy = "https-only"
+    cache_policy_id  = data.aws_cloudfront_cache_policy.cacheDisable.id
   }
 
   # Cache behavior with precedence 0
@@ -41,30 +32,10 @@ resource "aws_cloudfront_distribution" "roboshop" {
     target_origin_id = "cdn.${var.zone_name}"
 
     viewer_protocol_policy = "https-only"
+    cache_policy_id  = data.aws_cloudfront_cache_policy.cacheEnable.id
   }
 
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "cdn.${var.zone_name}"
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  price_class = "PriceClass_200"
+   price_class = "PriceClass_200"
 
   restrictions {
     geo_restriction {
@@ -73,11 +44,27 @@ resource "aws_cloudfront_distribution" "roboshop" {
     }
   }
 
-  tags = {
-    Environment = "production"
-  }
+  tags = merge(
+    local.common_tags,{
+        Name = "${var.project}-${var.environment}"
+    }
+  )
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = local.acm_certificate_arn
+    ssl_support_method = "sni-only"
+  }
+}
+
+
+resource "aws_route53_record" "frontend_alb" {
+  zone_id = var.zone_id
+  name    = "cdn.${var.zone_name}" #dev.daws84s.site
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.roboshop.domain_name
+    zone_id                = aws_cloudfront_distribution.roboshop.hosted_zone_id
+    evaluate_target_health = true
   }
 }
